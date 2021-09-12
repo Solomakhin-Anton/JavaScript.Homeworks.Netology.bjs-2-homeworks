2a7fba119792741d2732dace0ce3fb1c75021dfc
Solomakhin Anton <s.anton45@yandex.ru>
2021-09-12 15:34:01 +0300

    2  done

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,46 @@
-// Задание 1
+// 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg;
 
-  // Ваш код
-
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
+    sum += arr[i];
+  }
+  avg = Number((sum / arr.length).toFixed(2));
   return { min:min, max:max, avg:avg };
 }
 
-// Задание 2
+// 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
+  let sum = 0;
+  
+  arr.forEach(item => sum += item);
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
-
-  // Ваш кода
-  // for ...
+  let max = -Infinity;
   
-  return max
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sum = func(arrOfArr[i]);
+    if (sum > max) max = sum;
+  }
+  return max;
 }
 
-// Задание 3
+// 3
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
+  }
+  return max - min;
 }
