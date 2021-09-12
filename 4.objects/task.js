6d3faf3b693241c93d5347b00d09f3870576f22a
Solomakhin Anton <s.anton45@yandex.ru>
2021-09-12 15:35:43 +0300

    4  done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,30 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  (this.marks === undefined) ? this.marks = [mark] : this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+    (this.marks === undefined) ? this.marks = [...marks] : this.marks.push(...marks);
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  this.marks.forEach(item => sum += item);
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
